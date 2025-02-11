a0ae08b25b4c8b5261d121f5e24a181c86c40ac8
Daovancong <congleesin2001@gmail.com>
2025-02-11 09:59:51 +0700

    upadte thong tin tuyen dung

--- src/app/recruitment/page.tsx
+++ src/app/recruitment/page.tsx
@@ -7,6 +7,29 @@ import BannerSlider from '@/components/BannerSlider';
 import Link from "next/link";
 
 export default function recruitment() {
+    const reacuitments = [
+        {
+            id: 1,
+            location: ' Intern Front End',
+            wage: 'Thỏa thuận',
+            address: 'Hà Nội',
+            time: ' Hạn hồ sơ 28/02/2025',
+        },
+        {
+            id: 2,
+            location: 'Intern Back End',
+            wage: 'Thỏa thuận',
+            address: 'Hà Nội',
+            time: ' Hạn hồ sơ 28/02/2025',
+        },
+        {
+            id: 3,
+            location: 'Intern Ai (Python)',
+            wage: 'Thỏa thuận',
+            address: 'Hà Nội',
+            time: ' Hạn hồ sơ 28/02/2025',
+        }
+    ];
     return (
         <main className={`${style.mainIndex} w-[70%] bg-white ml-[15%] mr-[15%] relative top-[15.5vh] flex flex-col shadow-md`}>
             <div className='w-full h-10 bg-[#005db2] flex items-center'>
@@ -41,61 +64,63 @@ export default function recruitment() {
                 </div>
                 <div className={`${style.container} w-full px-7 py-4 mx-auto`}>
                     <div className='justify-center flex flex-wrap'>
-                        <div className='p-6 mb-4 cursor-default w-full mx-auto' style={{ borderBottom: '1px solid #e0e0e0' }}>
-                            <div className='flex flex-wrap'>
-                                <div className='pl-0 flex-col flex flex-grow relative w-full pr-4'>
-                                    <div>
-                                        <Link href={'#'} className='font-[700] no-underline '>
-                                            Intern Front End
+                        {reacuitments.map(recruitment => (
+                            <div key={recruitment.id} className='p-6 mb-4 cursor-default w-full mx-auto' style={{ borderBottom: '1px solid #e0e0e0' }}>
+                                <div className='flex flex-wrap'>
+                                    <div className='pl-0 flex-col flex flex-grow relative pr-4 w-1/2'>
+                                        <div>
+                                            <Link href={'#'} className='font-[700] no-underline '>
+                                                {recruitment.location}
+                                            </Link>
+                                        </div>
+                                        <div className='flex flex-col pt-2'>
+                                            <div className='pr-2 pt-3 flex flex-col '>
+                                                <div className='flex flex-row'>
+                                                    <span className='inline-block w-6 h-6'>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
+                                                        </svg>
+                                                    </span>
+                                                    <span className='text-red-500 ml-2 mr-12 text-base whitespace-nowrap flex flex-col justify-center'>
+                                                        {recruitment.wage}
+                                                    </span>
+                                                </div>
+                                            </div>
+                                            <div className='pr-2 pt-3 flex flex-col '>
+                                                <div className='flex flex-row'>
+                                                    <span className='inline-block w-6 h-6'>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                                                        </svg>
+                                                    </span>
+                                                    <span className='text-black ml-2 mr-12 text-base whitespace-nowrap flex flex-col justify-center'>
+                                                        {recruitment.address}
+                                                    </span>
+                                                </div>
+                                            </div>
+                                            <div className='pr-2 pt-3 flex flex-col '>
+                                                <div className='flex flex-row'>
+                                                    <span className='inline-block w-6 h-6'>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                                        </svg>
+                                                    </span>
+                                                    <span className='text-black ml-2 mr-12 text-base whitespace-nowrap flex flex-col justify-center'>
+                                                        {recruitment.time}
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='justify-center flex-col flex '>
+                                        <Link href={'#'} className='w-full block no-underline p-3 my-auto text-white text-center whitespace-nowrap' style={{ background: '#db0d0d', maxWidth: '200px', minWidth: '100px', borderRadius: '24px' }}>
+                                            Ứng tuyển
                                         </Link>
                                     </div>
-                                    <div className='flex flex-col pt-2'>
-                                        <div className='pr-2 pt-3 flex flex-col '>
-                                            <div className='flex flex-row'>
-                                                <span className='inline-block w-6 h-6'>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
-                                                    </svg>
-                                                </span>
-                                                <span className='text-red-500 ml-2 mr-12 text-base whitespace-nowrap flex flex-col justify-center'>
-                                                    Thỏa thuận
-                                                </span>
-                                            </div>
-                                        </div>
-                                        <div className='pr-2 pt-3 flex flex-col '>
-                                            <div className='flex flex-row'>
-                                                <span className='inline-block w-6 h-6'>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-                                                    </svg>
-                                                </span>
-                                                <span className='text-black ml-2 mr-12 text-base whitespace-nowrap flex flex-col justify-center'>
-                                                    Hà Nội
-                                                </span>
-                                            </div>
-                                        </div>
-                                        <div className='pr-2 pt-3 flex flex-col '>
-                                            <div className='flex flex-row'>
-                                                <span className='inline-block w-6 h-6'>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                                                    </svg>
-                                                </span>
-                                                <span className='text-black ml-2 mr-12 text-base whitespace-nowrap flex flex-col justify-center'>
-                                                    Hạn hồ sơ 28/02/2025
-                                                </span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className='justify-center flex-col flex '>
-                                    <Link href={'#'} className='w-full block no-underline p-3 my-auto text-white text-center whitespace-nowrap' style={{ background: '#db0d0d', maxWidth: '200px', minWidth: '100px', borderRadius: '24px' }}>
-                                        Ứng tuyển
-                                    </Link>
                                 </div>
                             </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
             </section>
