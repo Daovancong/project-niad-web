4f1ba24f25b9b9b2c96b40e379f112aac258a831
Daovancong <congleesin2001@gmail.com>
2025-02-26 09:52:18 +0700

    update baner left / baner right

--- src/components/bannder/BannderLeft.tsx
+++ src/components/bannder/BannderLeft.tsx
@@ -7,24 +7,29 @@ export default function BannderLeft() {
     return (
         <aside className={`${style.banerLeft} w-[15%] top-[20vh] bg-gray-100 fixed left-0 h-[100vh] z-[99]`}>
             <div>
-                <div className='p-2 !pt-0'>
-                    <Link href={'#'}>
-                        <Image className='w-full h-[27.5vh]' width={700} height={50} src="/banner/baner-left01.png" alt="image banner left" />
-                        <div>
-                            {/* <h1 className='font-bold'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, nihil? Sed, voluptatum!
-                        </h1> */}
+                <div className='p-2 !pt-0 mb-2'>
+                    <Link href={'https://bic.vn/noi-dung/ban-tin-bic/2025-bic-vuon-minh-but-pha-huong-toi-ky-niem-20-nam-thanh-lap.html'}>
+                        <Image className='w-full h-[20vh]' width={300} height={300} src="/banner/2025-bic-vuon-minh-but-pha-huong-toi-ky-niem-20-nam-thanh-lap.png" alt="image banner left" />
+                        <div className='mt-3'>
+                            <div className='text-sm text-gray-500 mb-2'>18.02.2025</div>
+                            <p className='text-ellipsis overflow-hidden text-base text-black'>
+                                2025: BIC vươn mình bứt phá hướng tới kỷ niệm 20 năm thành lập.
+                            </p>
                         </div>
                     </Link>
-
                 </div>
                 <div className='p-2 !pt-0'>
-                    <Image className='w-full h-[27.5vh]' width={700} height={70} src="/banner/baner-left2.png" alt="image banner left" />
-                </div>
-                <div className='p-2 !pt-0'>
-                    <Image className='w-full h-[27.5vh]' width={700} height={70} src="/banner/baner-left01.png" alt="image banner left" />
+                    <Link href={'https://bic.vn/noi-dung/ban-tin-bic/bic-tang-phong-hoc-cho-truong-mam-non-binh-luong.html'}>
+                        <Image className='w-full h-[20vh]' width={300} height={300} src="/banner/bic-tang-phong-hoc-cho-truong-mam-non-binh-luong.png" alt="image banner left" />
+                    </Link>
+                    <div className='mt-3'>
+                        <div className='text-sm text-gray-500 mb-2'>23.01.2025</div>
+                        <p className='text-ellipsis overflow-hidden text-base text-black'>
+                            BIC tặng phòng học cho trường Mầm non Bình Lương
+                        </p>
+                    </div>
                 </div>
             </div>
-        </aside>
+        </aside >
     )
 }
--- src/components/bannder/BannderRight.tsx
+++ src/components/bannder/BannderRight.tsx
@@ -7,18 +7,27 @@ export default function BannderRight() {
     return (
         <aside className={`${style.banerRight} w-[15%] top-[20vh] bg-gray-100 fixed right-0 h-[85%] z-[999]`}>
             <div>
-                <div className='p-2 !pt-0'>
+                <div className='p-2 !pt-0 mb-2'>
                     <Link href={'#'}>
-                        <Image className='w-full h-[27.5vh]' width={700} height={70} src="/banner/baner-left01.png" alt="image banner left" />
-                        <div>
+                        <Image className='w-full h-[20vh]' width={700} height={70} src="/banner/bic-smart-care-bao-hiem-suc-khoe-cao-cap.jpg" alt="image banner left" />
+                        <div className='mt-3'>
+                            <div className='text-sm text-gray-500 mb-2'>18.02.2025</div>
+                            <p className='text-ellipsis overflow-hidden text-base text-black'>
+                                BIC Smart Care - Bảo hiểm sức khỏe cao cấp - Sống đẳng cấp, trịn an tâm
+                            </p>
                         </div>
                     </Link>
                 </div>
                 <div className='p-2 !pt-0'>
-                    <Image className='w-full h-[27.5vh]' width={700} height={70} src="/banner/baner-left01.png" alt="image banner left" />
-                </div>
-                <div className='p-2 !pt-0'>
-                    <Image className='w-full h-[27.5vh] ' width={700} height={70} src="/banner/baner-left01.png" alt="image banner left" />
+                    <Link href={'#'}>
+                        <Image className='w-full h-[20vh]' width={700} height={70} src="/banner/bao-hiem-y-te-bo-tro.jpg" alt="image banner left" />
+                        <div className='mt-3'>
+                            <div className='text-sm text-gray-500 mb-2'>18.02.2025</div>
+                            <p className='text-ellipsis overflow-hidden text-base text-black'>
+                                Bảo hiểm y tế hỗ trợ - Gạt bỏ lo âu, an âm điều trị
+                            </p>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </aside>
